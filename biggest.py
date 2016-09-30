2925486794af551907b17742b09d37aea2e25963
ThomasPatterson2002 <thomas.patterson18@venerablebede.co.uk>
2016-09-30 18:24:50 +0100

    Update biggest.py

--- biggest.py
+++ biggest.py
@@ -26,13 +26,13 @@ def run_tests():
 # Edit this code
 def biggest(a, b, c):
 
-    if a > b and a > c:
-        print (a)
+    if a >== b and a >== c:
+        print (a b c)
 
-    elif b > a and b > c:
-        print (b)
+    elif b >== a and b >== c:
+        print (b a c)
 
-    elif c > a and c > b:
-        print (c)
+    elif c >== a and c >== b:
+        print (c a b)
         
 
